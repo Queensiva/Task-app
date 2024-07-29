560bb6553262bdd610c1523dc38a724e02d2fa06
Queensiva <qsiva111@gmail.com>
2024-07-29 13:52:32 +0530

    Update models.py

--- sample/newapp/models.py
+++ sample/newapp/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Member(models.Model):
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=100)
-    duedate=models.DateTimeField()
+    duedate=models.DateTimeField(null=True,blank=True)
     priority=models.CharField(max_length=100)
     status = models.CharField(max_length=100)
